549985dba514964e4e6f15d1e854e31f5b69266e
Matheus Goncalves <MatGon.dev@gmail.com>
2019-12-05 18:25:57 -0500

    Security
    Removed SQL Vulnerability

--- app.py
+++ app.py
@@ -128,8 +128,7 @@ def resetpass():
 
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        txtSQL = "SELECT * FROM users WHERE email='" + email + "';"
-        cursor.execute(txtSQL)
+        cursor.execute('SELECT * FROM users WHERE email = %s', [email])
         account = cursor.fetchone()
         # If account exists show error and validation checks
         if account:
